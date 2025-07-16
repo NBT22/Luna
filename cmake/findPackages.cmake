3b5d16a088ef73c585455570a18efab968aeb39c
NBT22 <91098724+NBT22@users.noreply.github.com>
2025-07-16 16:53:20 -0500

    cmake improvements

--- cmake/findPackages.cmake
+++ cmake/findPackages.cmake
@@ -1,100 +1,75 @@
 include(FetchContent)
 include(CheckIncludeFile)
 
-function(getLatestPackageVersion gitRepo versionSplat)
+macro(getLatestPackageVersion gitRepo versionSplat)
     find_package(Git 2.18 REQUIRED)
     if (WIN32)
         execute_process(COMMAND powershell -command "((& '${GIT_EXECUTABLE}' -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort=version:refname --tags ${gitRepo} '${versionSplat}' | Select-Object -Last 1) -Split '/')[2]" OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE LATEST_RELEASE)
     else ()
         execute_process(COMMAND ${GIT_EXECUTABLE} -c "versionsort.suffix=-" ls-remote --exit-code --refs --sort=version:refname --tags ${gitRepo} "${versionSplat}" COMMAND tail --lines=1 COMMAND cut --delimiter=/ --fields=3 COMMAND tr -d "\n" OUTPUT_VARIABLE LATEST_RELEASE)
     endif ()
-    set(LATEST_RELEASE ${LATEST_RELEASE} PARENT_SCOPE)
-endfunction()
+endmacro()
 
-function(findVulkanHeaders)
-    find_package(Vulkan QUIET)
-    if (NOT Vulkan_FOUND)
-        getLatestPackageVersion(https://github.com/KhronosGroup/Vulkan-Headers.git v1.4.*)
+macro(makePackageAvailable gitRepo versionSplat packageName)
+    getLatestPackageVersion(${gitRepo} ${versionSplat})
 
-        FetchContent_Declare(
-                Headers
-                GIT_REPOSITORY https://github.com/KhronosGroup/Vulkan-Headers.git
-                GIT_TAG ${LATEST_RELEASE}
-                GIT_SHALLOW TRUE
-                GIT_PROGRESS TRUE
-        )
-        FetchContent_MakeAvailable(Headers)
-        set(Vulkan_INCLUDE_DIRS "${VULKAN_HEADERS_SOURCE_DIR}/include")
-    endif ()
-    set(Vulkan_INCLUDE_DIRS "${Vulkan_INCLUDE_DIRS}" PARENT_SCOPE)
-endfunction()
+    FetchContent_Declare(
+            ${packageName}
+            GIT_REPOSITORY ${gitRepo}
+            GIT_TAG ${LATEST_RELEASE}
+            GIT_SHALLOW TRUE
+            GIT_PROGRESS TRUE
+            EXCLUDE_FROM_ALL
+            SYSTEM
+            FIND_PACKAGE_ARGS ${ARGN}
+    )
+    FetchContent_MakeAvailable(${packageName})
+endmacro()
 
-function(findVulkan)
+macro(findVulkan)
     find_package(Vulkan QUIET)
     add_library(VulkanLibrary INTERFACE)
     if (NOT Vulkan_FOUND)
-        getLatestPackageVersion(https://github.com/KhronosGroup/Vulkan-Headers.git v1.4.*)
+        makePackageAvailable(https://github.com/KhronosGroup/Vulkan-Headers.git v1.4.* Headers)
+        makePackageAvailable(https://github.com/KhronosGroup/Vulkan-Loader.git v1.4.* Loader)
 
-        FetchContent_Declare(
-                Headers
-                GIT_REPOSITORY https://github.com/KhronosGroup/Vulkan-Headers.git
-                GIT_TAG ${LATEST_RELEASE}
-                GIT_SHALLOW TRUE
-                GIT_PROGRESS TRUE
-        )
-        FetchContent_Declare(
-                Loader
-                GIT_REPOSITORY https://github.com/KhronosGroup/Vulkan-Loader.git
-                GIT_TAG ${LATEST_RELEASE}
-                GIT_SHALLOW TRUE
-                GIT_PROGRESS TRUE
-        )
-        FetchContent_MakeAvailable(Headers Loader)
-
-        target_include_directories(VulkanLibrary INTERFACE ${VULKAN_HEADERS_SOURCE_DIR}/include ${VULKAN_LOADER_SOURCE_DIR}/loader)
+        target_compile_options(VulkanLibrary INTERFACE "SHELL:-isystem ${VULKAN_LOADER_SOURCE_DIR}/loader")
         target_link_libraries(VulkanLibrary INTERFACE Vulkan::Loader)
+        target_include_directories(VulkanLibrary INTERFACE ${VULKAN_HEADERS_SOURCE_DIR}/include ${VULKAN_LOADER_SOURCE_DIR}/loader)
     else ()
-        target_include_directories(VulkanLibrary INTERFACE ${Vulkan_INCLUDE_DIRS})
         target_link_libraries(VulkanLibrary INTERFACE Vulkan::Vulkan)
+        target_include_directories(VulkanLibrary INTERFACE ${Vulkan_INCLUDE_DIRS})
     endif ()
-endfunction()
+endmacro()
 
-function(findSDL3)
-    find_package(SDL3 CONFIG QUIET)
-    if (NOT SDL3_FOUND)
-        getLatestPackageVersion(https://github.com/libsdl-org/SDL.git release-3.*.*)
+macro(findSDL3)
+    makePackageAvailable(https://github.com/libsdl-org/SDL.git release-3.*.* SDL3 CONFIG REQUIRED)
+endmacro()
 
-        FetchContent_Declare(
-                SDL3
-                GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-                GIT_TAG ${LATEST_RELEASE}
-                GIT_SHALLOW TRUE
-                GIT_PROGRESS TRUE
-        )
-        FetchContent_MakeAvailable(SDL3)
+macro(fetchVMA)
+    check_include_file("vk_mem_alloc.h" VMA_FOUND)
+    add_library(VMA INTERFACE)
+    if (NOT VMA_FOUND)
+        makePackageAvailable(https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator.git v3.*.* vma)
+        target_compile_options(VMA INTERFACE "SHELL:-isystem ${vma_SOURCE_DIR}/include")
+        target_include_directories(VMA INTERFACE "${vma_SOURCE_DIR}/include")
     endif ()
-endfunction()
+endmacro()
 
-function(fetchCglm)
+macro(fetchCglm)
     check_include_file("cglm/cglm.h" cglm_FOUND)
     if (NOT cglm_FOUND)
-        getLatestPackageVersion(https://github.com/recp/cglm.git v0.*.*)
-
-        FetchContent_Declare(
-                cglm
-                GIT_REPOSITORY https://github.com/recp/cglm.git
-                GIT_TAG ${LATEST_RELEASE}
-                GIT_SHALLOW TRUE
-                GIT_PROGRESS TRUE
-        )
-        FetchContent_MakeAvailable(cglm)
-    else()
+        makePackageAvailable(https://github.com/recp/cglm.git v0.*.* cglm)
+    else ()
         add_library(cglm INTERFACE)
     endif ()
-endfunction()
+endmacro()
 
-function(fetchLodepng)
+macro(fetchLodePNG)
     file(DOWNLOAD https://raw.githubusercontent.com/lvandeve/lodepng/refs/heads/master/lodepng.cpp ${CMAKE_BINARY_DIR}/_deps/lodepng/lodepng.c)
     file(DOWNLOAD https://raw.githubusercontent.com/lvandeve/lodepng/refs/heads/master/lodepng.h ${CMAKE_BINARY_DIR}/_deps/lodepng/lodepng.h)
-    set(LODEPNG_DIR "${CMAKE_BINARY_DIR}/_deps/lodepng" PARENT_SCOPE)
-endfunction()
+    add_library(LodePNG INTERFACE)
+    target_compile_options(LodePNG INTERFACE "SHELL:-isystem ${CMAKE_BINARY_DIR}/_deps/lodepng")
+    target_sources(LodePNG INTERFACE "${CMAKE_BINARY_DIR}/_deps/lodepng/lodepng.c")
+    target_include_directories(LodePNG INTERFACE "${CMAKE_BINARY_DIR}/_deps/lodepng")
+endmacro()
