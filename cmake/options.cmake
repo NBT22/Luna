4832af6590eac1017db9bfa1517a64fad0b789fc
NBT22 <91098724+NBT22@users.noreply.github.com>
2025-11-16 22:46:18 -0600

    Compile for arm

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,7 +1,7 @@
 option(LUNA_USE_PIPES "Enable the usage of pipes in GCC, decreasing compile time at the cost of higher RAM usage when compiling" ON)
 option(LUNA_ENABLE_LTO "Enable LTO on release builds, which can increase performance at the cost of slower compile time and increased RAM usage when compiling" ON)
 
-option(LUNA_WARNINGS_ARE_FATAL "Treat warnings as errors, causing the build to fail if any warnings are present" ON)
+option(LUNA_WARNINGS_ARE_FATAL "Treat warnings as errors, causing the build to fail if any warnings are present" OFF)
 
 option(LUNA_DEFINE_VK_NO_PROTOTYPES "Define the `VK_NO_PROTOTYPES` macro, which allows the application to include <vulkan/vulkan_core.h> instead of <volk.h>" ON)
 
